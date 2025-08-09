917ef37cc77906a4c7c7c158830f9856212f97af
blueniarb <blueniarb@gmail.com>
2025-08-10 00:09:11 +0800

    Add refresh for#dev

--- assets/sw.js
+++ assets/sw.js
@@ -15,17 +15,11 @@ self.addEventListener('install', function (e) {
   );
 });
 
-/* Serve cached content when offline, but skip if #dev is in the URL */
+/* Serve cached content when offline */
 self.addEventListener('fetch', function (e) {
-  // Check if the request URL contains '#dev'
-  if (e.request.url.includes('#dev')) {
-    // Always fetch from network, skip cache
-    e.respondWith(fetch(e.request));
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(function (response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
 });
